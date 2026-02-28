d97f3a1751d812f6e828bb9602530a8e4c1b8b38
M Saad Mustafa <msaadmustafa091@gmail.com>
2026-02-28 09:11:47 -0800

    changes

--- backend/middleware/Auth.js
+++ backend/middleware/Auth.js
@@ -1,45 +1,88 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/User.model.js";
+// const verifyJwt = async (req, res, next) => {
+//   try {
+//     const token =
+//       req.cookies?.accessToken ||
+//       req.header["authorization"]?.replace("Bearer ", "");
+
+//     if (!token) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "Token Not FOund",
+//       });
+//     }
+
+//     const decodedToken = await jwt.verify(
+//       token,
+//       process.env.ACCESS_TOKEN_SECRET,
+//     );
+
+//     const user = await User.findById(decodedToken._id);
+
+//     if (!user) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "Invalid access Token",
+//       });
+//     }
+//     req.user = {
+//       _id: user._id,
+//       name: user.fullName,
+//       email: user.email,
+//       role: user.role, // 🔥 Ye line must hai
+//     };
+
+//     next();
+//   } catch (error) {
+//     console.log(error);
+
+//     return res.status(500).json({
+//       success: false,
+//       message: "Server Error",
+//     });
+//   }
+// };
 const verifyJwt = async (req, res, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
-      req.header["authorization"]?.replace("Bearer ", "");
+      req.headers.authorization?.replace("Bearer ", "");
 
     if (!token) {
-      return res.status(400).json({
+      return res.status(401).json({
         success: false,
-        message: "Token Not FOund",
+        message: "Token Not Found",
       });
     }
 
-    const decodedToken = await jwt.verify(
+    const decodedToken = jwt.verify(
       token,
-      process.env.ACCESS_TOKEN_SECRET,
+      process.env.ACCESS_TOKEN_SECRET
     );
 
     const user = await User.findById(decodedToken._id);
 
     if (!user) {
-      return res.status(400).json({
+      return res.status(401).json({
         success: false,
         message: "Invalid access Token",
       });
     }
+
     req.user = {
       _id: user._id,
       name: user.fullName,
       email: user.email,
-      role: user.role, // 🔥 Ye line must hai
+      role: user.role,
     };
 
     next();
   } catch (error) {
-    console.log(error);
-
-    return res.status(500).json({
+    console.log("JWT Error:", error);
+    return res.status(401).json({
       success: false,
-      message: "Server Error",
+      message: "Unauthorized",
     });
   }
 };
