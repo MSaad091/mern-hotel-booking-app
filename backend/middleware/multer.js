8d215082ff96b712afd5cb428c37770d4702be0b
M Saad Mustafa <msaadmustafa091@gmail.com>
2026-03-01 08:51:51 -0800

    update code

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,20 +1,33 @@
-import multer from 'multer'
-import path from 'path'
+// import multer from 'multer'
+// import path from 'path'
 
 
-const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null,path.join(process.cwd(), 'public','temp'))
-    },
-    filename: (req,file,cb) => {
-        const uniqueSuffix = Date.now( )  + '_'+ Math.round(Math.random() * 1E9)
-        cb(null,uniqueSuffix + '_' + file.originalname)
+// const storage = multer.diskStorage({
+//     destination: (req,file,cb) => {
+//         cb(null,path.join(process.cwd(), 'public','temp'))
+//     },
+//     filename: (req,file,cb) => {
+//         const uniqueSuffix = Date.now( )  + '_'+ Math.round(Math.random() * 1E9)
+//         cb(null,uniqueSuffix + '_' + file.originalname)
+//     }
+// })
+
+// const upload = multer({storage})
+
+// export {upload}
+
+
+
+import multer from 'multer';
+
+// Render ke liye memoryStorage best hai kyunki disk par file save karne ki permission nahi chahiye hoti
+const storage = multer.memoryStorage();
+
+const upload = multer({ 
+    storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024 // 5MB limit
     }
-})
+});
 
-const upload = multer({storage})
-
-export {upload}
-
-
-
+export { upload };
